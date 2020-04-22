a072d26b8eee82d14b8573590b47b01ea1e4a0ae
vijay <54017399+vijay8432@users.noreply.github.com>
2020-04-22 15:19:16 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,11 +1,12 @@
 define([
     'postmonger'
-], function(
+], function (
     Postmonger
 ) {
     'use strict';
 
     var connection = new Postmonger.Session();
+    var authTokens = {};
     var payload = {};
     var lastStepEnabled = false;
     var steps = [ // initialize to the same value as what's set in config.json for consistency
@@ -22,17 +23,14 @@ define([
     connection.on('requestedTokens', onGetTokens);
     connection.on('requestedEndpoints', onGetEndpoints);
 
-    connection.on('clickedNext', onClickedNext);
-    connection.on('clickedBack', onClickedBack);
-    connection.on('gotoStep', onGotoStep);
-
+    connection.on('clickedNext', save);
+   
     function onRender() {
         // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
 
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
-
         // Disable the next button if a value isn't selected
         $('#select1').change(function() {
             var message = getMessage();
@@ -51,12 +49,12 @@ define([
         });
     }
 
-    function initialize (data) {
+    function initialize(data) {
+        console.log(data);
         if (data) {
             payload = data;
         }
-
-        var message;
+        
         var hasInArguments = Boolean(
             payload['arguments'] &&
             payload['arguments'].execute &&
@@ -66,51 +64,42 @@ define([
 
         var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-        $.each(inArguments, function(index, inArgument) {
-            $.each(inArgument, function(key, val) {
-                if (key === 'message') {
-                    message = val;
-                }
+        console.log(inArguments);
+            var message;
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
+            if (key === 'message') {
+                message = val;
+            }
+              
             });
         });
-
-        // If there is no message selected, disable the next button
         if (!message) {
             showStep(null, 1);
-            connection.trigger('updateButton', { button: 'next', enabled: false });
+            connection.trigger('updateButton', {
+                button: 'next',
+                text: 'done',
+                visible: true
+            });
             // If there is a message, skip to the summary step
         } else {
             $('#select1').find('option[value='+ message +']').attr('selected', 'selected');
             $('#message').html(message);
             showStep(null, 3);
         }
+
+       
+
     }
 
-    function onGetTokens (tokens) {
-        // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
-        // console.log(tokens);
+    function onGetTokens(tokens) {
+        console.log(tokens);
+        authTokens = tokens;
     }
 
-    function onGetEndpoints (endpoints) {
-        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
-        // console.log(endpoints);
+    function onGetEndpoints(endpoints) {
+        console.log(endpoints);
     }
-
-    function onClickedNext () {
-        if (
-            (currentStep.key === 'step3' && steps[3].active === false) ||
-            currentStep.key === 'step4'
-        ) {
-            save();
-        } else {
-            connection.trigger('nextStep');
-        }
-    }
-
-    function onClickedBack () {
-        connection.trigger('prevStep');
-    }
-
     function onGotoStep (step) {
         showStep(step);
         connection.trigger('ready');
@@ -174,26 +163,21 @@ define([
                 break;
         }
     }
-
     function save() {
-        var name = $('#select1').find('option:selected').html();
-        var value = getMessage();
+        var postcardURLValue = $('#postcard-url').val();
+        var postcardTextValue = $('#postcard-text').val();
 
-        // 'payload' is initialized on 'initActivity' above.
-        // Journey Builder sends an initial payload with defaults
-        // set by this activity's config.json file.  Any property
-        // may be overridden as desired.
-        payload.name = name;
-
-        payload['arguments'].execute.inArguments = [{ "message": value }];
-
+        payload['arguments'].execute.inArguments = [{
+            "tokens": authTokens,
+            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}",
+            "phoneNumber": "{{Contact.Attribute.PostcardJourney.phoneNumber}}"
+        }];
+        
         payload['metaData'].isConfigured = true;
 
+        console.log(payload);
         connection.trigger('updateActivity', payload);
     }
 
-    function getMessage() {
-        return $('#select1').find('option:selected').attr('value').trim();
-    }
 
 });
