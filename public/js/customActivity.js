0f49f701560fa16d15c3732ee7255d8ecdc4bcd5
vijay <54017399+vijay8432@users.noreply.github.com>
2020-04-22 17:33:00 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -8,15 +8,6 @@ define([
     var connection = new Postmonger.Session();
     var authTokens = {};
     var payload = {};
-    var lastStepEnabled = false;
-    var steps = [ // initialize to the same value as what's set in config.json for consistency
-        { "label": "Step 1", "key": "step1" },
-        { "label": "Step 2", "key": "step2" },
-        { "label": "Step 3", "key": "step3" },
-        { "label": "Step 4", "key": "step4", "active": false }
-    ];
-    var currentStep = steps[0].key;
-
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
@@ -27,27 +18,11 @@ define([
    
     function onRender() {
         // JB will respond the first time 'ready' is called with 'initActivity'
-       
-        // Disable the next button if a value isn't selected
-        $('#select1').change(function() {
-            var message = getMessage();
-            connection.trigger('updateButton', { button: 'next', enabled: Boolean(message) });
-
-            $('#message').html(message);
-        });
-
-        // Toggle step 4 active/inactive
-        // If inactive, wizard hides it and skips over it during navigation
-        $('#toggleLastStep').click(function() {
-            lastStepEnabled = !lastStepEnabled; // toggle status
-            steps[3].active = !steps[3].active; // toggle active
-
-            connection.trigger('updateSteps', steps);
-        });
         connection.trigger('ready');
 
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
+
     }
 
     function initialize(data) {
@@ -66,31 +41,19 @@ define([
         var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
         console.log(inArguments);
-            var message;
+
         $.each(inArguments, function (index, inArgument) {
             $.each(inArgument, function (key, val) {
-            if (key === 'message') {
-                message = val;
-            }
+                
               
             });
         });
-        if (!message) {
-            showStep(null, 1);
-            connection.trigger('updateButton', {
-                button: 'next',
-                text: 'done',
-                visible: true
-            });
-            // If there is a message, skip to the summary step
-        } else {
-            $('#select1').find('option[value='+ message +']').attr('selected', 'selected');
-            $('#message').html(message);
-            showStep(null, 3);
-        }
 
-       
-
+        connection.trigger('updateButton', {
+            button: 'next',
+            text: 'done',
+            visible: true
+        });
     }
 
     function onGetTokens(tokens) {
@@ -101,88 +64,25 @@ define([
     function onGetEndpoints(endpoints) {
         console.log(endpoints);
     }
-    function onGotoStep (step) {
-        showStep(step);
-        connection.trigger('ready');
-    }
 
-    function showStep(step, stepIndex) {
-        if (stepIndex && !step) {
-            step = steps[stepIndex-1];
-        }
-
-        currentStep = step;
-
-        $('.step').hide();
-
-        switch(currentStep.key) {
-            case 'step1':
-                $('#step1').show();
-                connection.trigger('updateButton', {
-                    button: 'next',
-                    enabled: Boolean(getMessage())
-                });
-                connection.trigger('updateButton', {
-                    button: 'back',
-                    visible: false
-                });
-                break;
-            case 'step2':
-                $('#step2').show();
-                connection.trigger('updateButton', {
-                    button: 'back',
-                    visible: true
-                });
-                connection.trigger('updateButton', {
-                    button: 'next',
-                    text: 'next',
-                    visible: true
-                });
-                break;
-            case 'step3':
-                $('#step3').show();
-                connection.trigger('updateButton', {
-                     button: 'back',
-                     visible: true
-                });
-                if (lastStepEnabled) {
-                    connection.trigger('updateButton', {
-                        button: 'next',
-                        text: 'next',
-                        visible: true
-                    });
-                } else {
-                    connection.trigger('updateButton', {
-                        button: 'next',
-                        text: 'done',
-                        visible: true
-                    });
-                }
-                break;
-            case 'step4':
-                $('#step4').show();
-                break;
-        }
-    }
     function save() {
         var postcardURLValue = $('#postcard-url').val();
         var postcardTextValue = $('#postcard-text').val();
 
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
-            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}",
-            "phoneNumber": "{{Contact.Attribute.PostcardJourney.phoneNumber}}"
+            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
+        }];
+         payload['arguments'].execute.outArguments = [{
+             "tokens": authTokens,
+            "foundSignupDate": 'THIS is output-200',
         }];
         
         payload['metaData'].isConfigured = true;
-        payload['arguments'].execute.outArguments = [{
-            "foundSignupDate": 'THIS is output-200',
-        }];
-        console.log('payload',payload);
+
+        console.log(payload);
         connection.trigger('updateActivity', payload);
     }
- function getMessage() {
-    return $('#select1').find('option:selected').attr('value').trim();
-}
+
 
 });
